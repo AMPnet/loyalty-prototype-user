cffb47c38f4f4762872c5f055898b9717e3f49d6
Domagoj Latečki <domagoj.latecki@gmail.com>
2022-03-22 11:48:44 +0100

    SD-621: add QR code timer

--- src/app/verification/generate-button/generate-button.component.ts
+++ src/app/verification/generate-button/generate-button.component.ts
@@ -1,8 +1,8 @@
-import {ApplicationRef, ChangeDetectionStrategy, Component} from '@angular/core';
+import {ApplicationRef, ChangeDetectionStrategy, Component, OnInit} from '@angular/core';
 import {AuthService, LoginState} from "../../login/auth-service/auth.service";
 import {HttpClient} from "@angular/common/http";
 import {switchMap, tap} from "rxjs/operators";
-import {BehaviorSubject, Observable} from "rxjs";
+import {BehaviorSubject, interval, Observable} from "rxjs";
 
 @Component({
   selector: 'app-generate-button',
@@ -10,12 +10,15 @@ import {BehaviorSubject, Observable} from "rxjs";
   styleUrls: ['./generate-button.component.css'],
   changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class GenerateButtonComponent {
+export class GenerateButtonComponent implements OnInit {
 
   loginState$: Observable<LoginState>
   signingState$: BehaviorSubject<SigningState>
   messageToSign$: BehaviorSubject<GenerateMessageResponse | null>
-  qrCodePayload: string
+  remainingTime$: BehaviorSubject<string>
+
+  signedPayload: string
+  countdownTime?: Date
 
   constructor(private authService: AuthService,
               private httpClient: HttpClient,
@@ -23,8 +26,35 @@ export class GenerateButtonComponent {
     this.loginState$ = this.authService.loginState$
     this.signingState$ = new BehaviorSubject<SigningState>("GENERATE_PAYLOAD")
     this.messageToSign$ = new BehaviorSubject<GenerateMessageResponse | null>(null)
+    this.remainingTime$ = new BehaviorSubject<string>("None")
     this.authService.account$.subscribe(() => this.signingState$.next("GENERATE_PAYLOAD"))
-    this.qrCodePayload = ""
+    this.signedPayload = ""
+  }
+
+  ngOnInit() {
+    interval(1000).subscribe(() => {
+      const now = new Date().getTime()
+      const target = this.countdownTime?.getTime()
+
+      this.remainingTime$.next("5:00")
+
+      if (!!target) {
+        const diff = target - now
+
+        if (diff > 0) {
+          const minutes = Math.floor(diff / 60000)
+          const seconds = Math.floor((diff - (minutes * 60000)) / 1000)
+
+          if (seconds >= 10) {
+            this.remainingTime$.next(`${minutes}:${seconds}`)
+          } else {
+            this.remainingTime$.next(`${minutes}:0${seconds}`)
+          }
+        } else {
+          this.remainingTime$.next("Expired")
+        }
+      }
+    })
   }
 
   generateMessage() {
@@ -50,7 +80,8 @@ export class GenerateButtonComponent {
         switchMap(signature => this.verifySignature(messageToSign.id, signature))
       ).subscribe((resp) => {
         this.signingState$.next("DISPLAY_CODE")
-        this.qrCodePayload = JSON.stringify(resp)
+        this.signedPayload = JSON.stringify(resp)
+        this.countdownTime = new Date(resp.valid_until)
       })
     }
   }
@@ -75,5 +106,5 @@ interface GenerateMessageResponse {
 interface VerifySignedMessageResponse {
   id: string
   signature: string
-  validUntil: string
+  valid_until: string
 }
